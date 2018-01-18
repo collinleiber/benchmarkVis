c941255e2b329ce4275b9a00cfd9668e27edea12
Collin Leiber <c.leiber@campus.lmu.de>
2018-01-18 23:52:46 +0100

    try fix #1

--- tests/testthat/test_checkCsvHandler.R
+++ tests/testthat/test_checkCsvHandler.R
@@ -1,14 +1,10 @@
 context("CSV Handler")
 
-#  ===================== Basic Setup =====================
-# Export and import csv
-csvExport(mlr.benchmark.example, "test.csv")
-df = csvImport("test.csv")
-file.remove("test.csv")
-#  =======================================================
-
 # Check if exporting and importing a csv file works properly
 test_that("CSV Export/Import Test", {
+  csvExport(mlr.benchmark.example, "test.csv")
+  df = csvImport("test.csv")
+  file.remove("test.csv")
   # Workarount to avoid difference between named empty list and list()
   df2 = cbind(mlr.benchmark.example)
   df2$algorithm.parameter = sapply(mlr.benchmark.example$algorithm.parameter, function(x) {
@@ -24,5 +20,9 @@ test_that("CSV Export/Import Test", {
 
 # Check dataframe structure
 test_that("CSV Import Structure", {
+  csvExport(mlr.benchmark.example, "test.csv")
+  df = csvImport("test.csv")
+  file.remove("test.csv")
+  # Check structure
   expect_true(checkStructure(df))
 })
