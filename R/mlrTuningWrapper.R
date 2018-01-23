4a6ec28773d909f93beb09a89636fce312533acf
Collin Leiber <c.leiber@campus.lmu.de>
2018-01-23 23:43:41 +0100

    add mlr tuning wrapper (work in progress)

---
+++ R/mlrTuningWrapper.R
@@ -0,0 +1,12 @@
+num_ps = makeParamSet( # algorithm parameter
+  makeNumericParam("C", lower = -10, upper = 10, trafo = function(x) 10^x),
+  makeNumericParam("sigma", lower = -10, upper = 10, trafo = function(x) 10^x)
+)
+ctrl = makeTuneControlRandom(maxit = 100L) # replication
+rdesc = makeResampleDesc("CV", iters = 3L) # algorithm parameter + iteration X
+res = tuneParams("classif.ksvm", task = iris.task, resampling = rdesc, par.set = num_ps, # Problem, Algorithm
+  control = ctrl, measures = list(acc, setAggregation(acc, test.sd)), show.info = FALSE)
+
+useMlrTuningWrapper = function(res) {
+
+}
