9c89cdd6f4c3e07a4e17107a77bf6a87c4842263
Alona Sydorova <alona.sydorova@gmail.com>
2018-01-26 11:47:38 +0100

    adjust style rankplot.R

--- R/RankPlot.R
+++ R/RankPlot.R
@@ -14,9 +14,9 @@
 createRankPlot = function(df, measure, problem, algorithm) {
   checkmate::assert_data_frame(df)
   if (length(df[, measure])) {
-    order.scores <- order(df[, measure])
-    df$rank <- NA
-    df$rank[order.scores] <- 1:nrow(df)
+    order.scores = order(df[, measure])
+    df$rank = NA
+    df$rank[order.scores] = seq_len(nrow(df))
     df$rank = as.factor(df$rank)
     p =  ggplot2::ggplot(df, ggplot2::aes(x = df$rank, problem, fill = algorithm)) +
       ggplot2::geom_tile(position = "dodge") +
