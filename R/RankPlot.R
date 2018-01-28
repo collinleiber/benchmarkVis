ed2ceaea88468554f3d5482bd3ca35e8f99e0950
likunlan <li.kunlan@campus.lmu.de>
2018-01-28 20:30:25 +0100

    updata Rankplot

--- R/RankPlot.R
+++ R/RankPlot.R
@@ -9,23 +9,24 @@
 #' @return a rank plot
 #' @export
 #' @import ggplot2
+#' @import dplyr
 #' @examples
 #' createRankPlot(mlr.benchmark.example, 'mmce.test.mean')
 createRankPlot = function(df, measure) {
   checkmate::assert_data_frame(df)
   if (length(df[, measure])) {
+
+    abcd = df %>% group_by(df$problem) %>% mutate(rank = order(eval(parse(text=sprintf('%s',measure)))))
+
     order.scores = order(df$problem, df[, measure])
-    #order.scores = order.scores%%3
     rank = NA
     rank[order.scores] = seq_len(nrow(df))
     rank = as.factor(rank)
-    print(rank)
-    p =  ggplot2::ggplot(df, ggplot2::aes(x = rank, df$problem, fill = df$algorithm)) +
-      ggplot2::geom_tile(position = "dodge") +
+    p =  ggplot2::ggplot(abcd, ggplot2::aes(x = rank, problem, fill = algorithm)) +
+      ggplot2::geom_tile() +
       ggplot2::labs(title = "Rank Plot",
                     x = "Rank",
                     y = "Task.id")
-    #  return(p)
     return(p)
   }
 }
