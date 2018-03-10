6f47962190ee1dd26431717eec4887e3b1923bc2
Alona Sydorova <alona.sydorova@gmail.com>
2018-03-10 17:49:52 +0100

    update structure of shiny tabs

---
+++ shiny/ui/plots.R
@@ -0,0 +1,32 @@
+tabpanel.plots =  list(
+    conditionalPanel("output.fileUploaded == true && input.Submit == true",
+        h3("Here you can create plots for your data"),
+        fluidRow(
+            column(4,
+                radioButtons("datatype", "Choose type of data to work with:",
+                           choices = c('submitted data', 'aggregated data'),
+                           selected = "submitted data"),
+                 selectInput(
+                        'plots',
+                        'Choose a plot',
+                        listPlots(),
+                        selected = FALSE,
+                        multiple = FALSE
+                        )
+            ),
+            column(4,
+                conditionalPanel("input.plots!=0",
+                box("TODO: select plot specific parameters (=what to plot)")
+                    #getPlotParameters(input$plots)                           
+                ),
+                actionButton("createplot", "Create plot", icon = icon("check"))          
+            ),
+            column(3,
+                conditionalPanel("input.plots != 0 && input.createplot != 0",
+                    actionButton("create.tab", "Create a new tab for this plot", icon = icon("check"))
+                )          
+            )
+        )
+        #TODO: conditional panel with plot
+    )
+)
--- shiny/ui/table.R
+++ shiny/ui/table.R
@@ -1,12 +1,18 @@
 require(data.table)
 tabpanel.table =  list(
     conditionalPanel("output.fileUploaded == true && input.Submit == true",
-        h3("Here you can aggregate and play with a table"),   
-        uiOutput("table.aggregation"),
-        actionButton("Aggregation", "Aggregation", icon = icon("check")),
-        actionButton("Reset", "Reset", icon = icon("check")),
-        uiOutput("table.transformation"),
-        actionButton("Transformation", "Transformation", icon = icon("check")),
+        h3("Here you can aggregate and play with a table"),
+        fluidRow(
+            column(3,
+                uiOutput("table.aggregation"),
+                actionButton("Aggregation", "Aggregation", icon = icon("check")),
+                actionButton("Reset", "Reset", icon = icon("check"))
+            ),
+            column(3,
+                uiOutput("table.transformation"),
+                actionButton("Transformation", "Transformation", icon = icon("check"))                
+            )
+        ),
         DT::dataTableOutput("DataTable")
     )
 )
