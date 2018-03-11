fea502a28f4e8b0db84d2b6df0f90b13ae0933bd
Alona Sydorova <alona.sydorova@gmail.com>
2018-03-11 21:42:20 +0100

    plots tab

--- shiny/server/plots.R
+++ shiny/server/plots.R
@@ -1,12 +1,17 @@
 current.data = reactiveValues(data = NULL)
 valid.plots = reactiveValues(plots = NULL)
-observeEvent(input$currentdata, {
-    if(input$currentdata == 'submitted data') {        
+current.plot = reactiveValues(func = NULL, parameter = NULL)
+observeEvent(input$current.data, {
+    if(input$current.data == 'submitted data') {        
         current.data$data = table$data
     }
     else {
         current.data$data = aggregated.data$dt
     }
+})
+observeEvent(input$plots, {
+    current.plot$func = input$plots
+    current.plot$parameter = as.list(args(input$plots))
 })
 
 output$plotselection = renderUI({
@@ -19,7 +24,68 @@ output$plotselection = renderUI({
     )
 })
 
+output$plot.parameter.selection = renderUI({
+    parameter = as.list(args(input$plots))
+    uilist = list()
+    for (i in 2:length(parameter)) {
+        #if(names(parameter[i]) == "dt")
+        #    next
+        uilist = c(uilist,getValidUI(parameter[i]))
+         observeEvent(input$paste('param_', names(parameter[i])), {
+            cat("klappt")
+        })
+    }    
+    do.call(tagList, uilist)   
+})
+
 output$plot = renderPlotly({
     parameter = c(dt = current.data$data, input$plotparameter)
-    do.call(eval(parse(text=input$plots)), parameter)
+    print(input$plots)
+    print(parameter)
+    print(typeof(parameter$dt))
+    do.call(eval(parse(text=input$plots)), list(parameter))
 })
+
+getValidUI = function(param) {
+    if (is.null(param) || is.null(names(param)) || is.null(param[[1]])) {
+        return()
+    }
+    
+    param.name = names(param)
+    if (grepl("measure", param.name) && !grepl("measures", param.name)) {
+        ui.elem = selectInput(
+            paste('param_', param.name),
+            paste('Choose ', param.name),
+            choices = getMeasures(current.data$data),
+            selected = FALSE,
+            multiple = FALSE
+        )
+    }
+    else if (grepl("measures", param.name)) {
+        ui.elem = selectInput(
+            paste('param_', param.name),
+            paste('Choose ', param.name),
+            choices = getMeasures(current.data$data),
+            selected = FALSE,
+            multiple = TRUE
+        )
+    }
+    else if (typeof(param[[1]]) == "logical") {
+        ui.elem = selectInput(
+            paste('param_', param.name),
+            paste('Choose ', param.name),
+            choices = c('TRUE', 'FALSE'),
+            selected = FALSE,
+            multiple = FALSE
+        )
+    }
+    else {
+        ui.elem = textInput(
+            paste('param_', param.name),
+            paste('Choose ', param.name),
+            ''
+        )
+    }
+           
+    return(ui.elem)
+}
--- shiny/ui/plots.R
+++ shiny/ui/plots.R
@@ -3,24 +3,24 @@ tabpanel.plots =  list(
         h3("Here you can create plots for your data"),
         fluidRow(
             column(4,
-                radioButtons("currentdata", "Choose type of data to work with:",
+                radioButtons("current.data", "Choose type of data to work with:",
                            choices = c('submitted data', 'aggregated data'),
                            selected = "submitted data"),
                 uiOutput("plotselection")
             ),
             column(4,
-                conditionalPanel("input.plots!=0",
-                box("TODO: select plot specific parameters (=what to plot)")
-                    #getPlotParameters(input$plots)                           
-                ),
+                uiOutput("plot.parameter.selection"),
                 actionButton("createplot", "Create plot", icon = icon("check"))          
             ),
             column(3,
-                conditionalPanel("input.plots != 0 && input.createplot != 0",
+                conditionalPanel("input.createplot == true",
                     actionButton("create.tab", "Create a new tab for this plot", icon = icon("check"))
                 )          
             )
-        )
+        ),
         #TODO: conditional panel with plot
+        conditionalPanel("input.createplot == true",
+            plotlyOutput("plot")
+        ) 
     )
 )
