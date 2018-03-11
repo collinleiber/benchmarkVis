f0211f7ff9025bc2e2fbe3d6f3e07f4be0d5b369
alona-sydorova <33294197+alona-sydorova@users.noreply.github.com>
2018-03-11 17:22:44 +0100

    Update and rename test_checkDotPlot.R to test_checkScatterPlot.R

--- tests/testthat/test_checkScatterPlot.R
+++ tests/testthat/test_checkScatterPlot.R
@@ -1,12 +1,12 @@
-context("Dot Plot")
+context("Scatter Plot")
 
-# Check if creating a Dot plot throws an error
+# Check if creating a Scatter plot throws an error
 test_that("createScatterPlot for mlr benchmark", {
   p = createScatterPlot(mlr.benchmark.example, "measure.mmce.test.mean", "measure.ber.test.mean")
   expect_true(is.list(p))
 })
 
-# Check if creating a Dot Plot throws an error
+# Check if creating a Scatter Plot throws an error
 test_that("createScatterPlot for microbenchmark", {
   p = createScatterPlot(microbenchmark.example, "measure.mean", "measure.median")
   expect_true(is.list(p))
