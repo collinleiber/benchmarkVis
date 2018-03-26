076b334900c6e8af68da246cc5bb83a6601451a0
Alona Sydorova <alona.sydorova@gmail.com>
2018-03-26 14:55:08 +0200

    compare plots shiny

---
+++ shiny/server/compareplots.R
@@ -0,0 +1,54 @@
+plots.to.rerender = reactiveValues(plot1 = NULL, plot2 = NULL) 
+
+observeEvent(input$savedPlot1, {
+    name = isolate(input$savedPlot1)
+    plots.to.rerender$plot1 = saved.plots$plotlist[[name]]
+})
+
+observeEvent(input$savedPlot2, {
+    name = isolate(input$savedPlot2)
+    plots.to.rerender$plot2 = saved.plots$plotlist[[name]]
+})
+
+output$selectPlot1 = renderUI({
+    selectInput(
+        'savedPlot1',
+        'Choose one of your saved plots',
+        choices = names(saved.plots$plotlist),
+        selected = FALSE,
+        multiple = FALSE
+    )
+})
+
+output$selectPlot2 = renderUI({
+    selectInput(
+        'savedPlot2',
+        'Choose one of your saved plots',
+        choices = names(saved.plots$plotlist),
+        selected = FALSE,
+        multiple = FALSE
+    )
+})
+
+output$rerenderPlot1 = renderUI({
+    plot = isolate(plots.to.rerender$plot1)
+    plot.type = class(plot)[1]
+    uiplot = switch(plot.type,
+        'plotly' = renderPlotly({plot}),
+        'chartJSRadar' = radarchart::chartJSRadarOutput(radarchart::renderChartJSRadar({plot}), width = "450", height = "300"),
+        renderText({"unknown plot format"})
+    )    
+    do.call(tagList, c(uiplot))
+})
+
+output$rerenderPlot2 = renderUI({
+    plot = isolate(plots.to.rerender$plot2)
+    plot.type = class(plot)[1]
+    uiplot = switch(plot.type,
+        'plotly' = renderPlotly({plot}),
+        'chartJSRadar' = radarchart::chartJSRadarOutput(radarchart::renderChartJSRadar({plot}), width = "450", height = "300"),
+        renderText({"unknown plot format"})
+    )    
+    do.call(tagList, c(uiplot))
+})
+
--- shiny/ui/compareplots.R
+++ shiny/ui/compareplots.R
@@ -1,3 +1,23 @@
 tabpanel.compareplots =  list(
-    
+    conditionalPanel("output.fileUploaded == true && input.Submit == true",
+        h3("Here you can compare two of your saved plots"),
+        conditionalPanel("output.plotsSaved == true",
+            fluidRow(
+                column(6,
+                    uiOutput("selectPlot1")
+                ),
+                column(6,
+                    uiOutput("selectPlot2") 
+                )
+            ),
+            fluidRow(
+                column(6,
+                    uiOutput("renderPlot1") 
+                ),
+                column(6,
+                    uiOutput("renderPlot2")  
+                )
+            )                  
+        ) 
+    )
 )
