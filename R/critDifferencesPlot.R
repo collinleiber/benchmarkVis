0bd46b973899f727ab080eba6f6d9a7c901badc1
likunlan <li.kunlan@campus.lmu.de>
2018-04-13 02:08:50 +0800

    update

--- R/critDifferencesPlot.R
+++ R/critDifferencesPlot.R
@@ -10,13 +10,14 @@
 #' @export
 #' @examples
 #' createCritDifferencesPlot(mlr.benchmark.example, "measure.mmce.test.mean")
-createCritDifferencesPlot = function(dt, measure, group.by = "problem") {
+createCritDifferencesPlot = function(dt, measure, group.by = "problem", test.str = "bd", p.value = 0.05) {
   # Checks
   checkmate::assert_data_table(dt)
   checkmate::assert_string(group.by)
   checkmate::assert_true(group.by %in% getMainColumns(dt))
   checkmate::assert_string(measure)
   checkmate::assert_true(measure %in% getMeasures(dt))
+  checkmate::assert_true(test.str %in% list("nemenyi", "bd"))
   #first part, genarate S3Obj object
   `%>%` = magrittr::`%>%`
   df = dt %>% dplyr::group_by(dt[[group.by]]) %>% dplyr::mutate(my_ranks = order(order(eval(parse(text = sprintf("%s", measure))), decreasing = TRUE)))
@@ -54,39 +55,80 @@ createCritDifferencesPlot = function(dt, measure, group.by = "problem") {
 
   aggr.meas = measure
 
-  data.table::setnames(pfdf, "tmpname", measure)
+  pfdf = data.table::setnames(pfdf, "tmpname", measure)
+
+  if (length(unique(pfdf$task.id)) < 2) {
+    stop("Benchmark results for at least two tasks are required")
+  }
+  if (length(unique(pfdf$learner.id)) < 2) {
+    stop("Benchmark results for at least two learners are required")
+  }
 
   nem.test = stats::friedman.test(as.formula(stringi::stri_paste(aggr.meas, " ~ learner.id | task.id",
                                                  sep = "")), data = pfdf)
-  test = "nemenyi"
+  #test = test.str
+
+  f.test = nem.test
+  n.learners = length(unique(pfdf$learner.id))
+  n.tasks = length(unique(pfdf$task.id))
+  if (!is.na(f.test$p.value)) {
+    f.rejnull = f.test$p.value < p.value
+    if (!f.rejnull)
+      warning("Cannot reject null hypothesis of overall Friedman test,\n             returning overall Friedman test.")
+  } else {
+    f.rejnull = FALSE
+    warning("P-value not computable. Learner performances might be exactly equal.")
+  }
+  q.nemenyi = qtukey(1 - p.value, n.learners, 1e+06)/sqrt(2L)
+  cd.nemenyi = q.nemenyi * sqrt(n.learners * (n.learners +
+                                                1L)/(6L * n.tasks))
+  q.bd = qtukey(1L - (p.value/(n.learners - 1L)), 2L, 1e+06)/sqrt(2L)
+  cd.bd = q.bd * sqrt(n.learners * (n.learners + 1L)/(6L *
+                                                        n.tasks))
+  if (f.rejnull) {
+    form = as.formula(stri_paste(aggr.meas, " ~ learner.id | task.id",
+                                 sep = ""))
+    nem.test = PMCMR::posthoc.friedman.nemenyi.test(form,
+                                                    data = df)
+    nem.test$crit.difference = list(nemenyi = cd.nemenyi,
+                                    bd = cd.bd)
+    nem.test$f.rejnull = f.rejnull
+    return(nem.test)
+  } else {
+    f.test$f.rejnull = f.rejnull
+    f.test$crit.difference = list(nemenyi = cd.nemenyi,
+                                  bd = cd.bd)
+  }
+  nem.test = f.test
 
   cd.info = list(
-    test = test,
-    cd = nem.test$crit.difference[[test]],
+    test = test.str,
+    cd = nem.test$crit.difference[[test.str]],
     x = cddf$mean.rank[cddf$learner.id == baseline],
     y = 0.1
   )
-  #if (test == "nemenyi") {
-  sub = sort(cddf$mean.rank)
-  mat = apply(
-    t(outer(sub, sub, `-`)),
-    c(1, 2),
-    FUN = function(x)
-      ifelse(x >
-               0 &&
-               x < cd.info$cd, x, 0)
-  )
-  xstart = round(apply(mat + sub, 1, min), 3)
-  xend = round(apply(mat + sub, 1, max), 3)
-  nem.df = data.table::data.table(xstart, xend, diff = xend - xstart)
-  nem.df = nem.df[, data.table::.SD[which.max(data.table::.SD$diff)], by = "xend"]
-  nem.df = nem.df[nem.df$xend - nem.df$xstart > 0, ]
-  nem.df$y = seq(from = 0.1,
-                 to = 0.35,
-                 length.out = dim(nem.df)[1])
-  cd.info$nemenyi.data = as.data.frame(nem.df)
-  #}
-  p.value = 0.05
+
+  if (test.str == "nemenyi") {
+    sub = sort(cddf$mean.rank)
+    mat = apply(
+      t(outer(sub, sub, `-`)),
+      c(1, 2),
+      FUN = function(x)
+        ifelse(x >
+                 0 &&
+                 x < cd.info$cd, x, 0)
+    )
+    xstart = round(apply(mat + sub, 1, min), 3)
+    xend = round(apply(mat + sub, 1, max), 3)
+    nem.df = data.table::data.table(xstart, xend, diff = xend - xstart)
+    nem.df = nem.df[, data.table::.SD[which.max(data.table::.SD$diff)], by = "xend"]
+    nem.df = nem.df[nem.df$xend - nem.df$xstart > 0,]
+    nem.df$y = seq(from = 0.1,
+                   to = 0.35,
+                   length.out = dim(nem.df)[1])
+    cd.info$nemenyi.data = as.data.frame(nem.df)
+  }
+
   obj = BBmisc::makeS3Obj(
     "CritDifferencesData",
     data = cddf,
--- tests/testthat/test_checkCritDifferencesPlot.R
+++ tests/testthat/test_checkCritDifferencesPlot.R
@@ -5,3 +5,15 @@ test_that("createCritDifferences for mlr benchmark", {
   p = createCritDifferencesPlot(mlr.benchmark.example, "measure.mmce.test.mean")
   expect_true(is.list(p))
 })
+
+# Check if creating a CritDifferences plot throws an error
+# test_that("createCritDifferences for mlr benchmark", {
+#   p = createCritDifferencesPlot(large.benchmark, "measure.mmce.test.mean")
+#   expect_true(is.list(p))
+# })
+
+# Check if creating a CritDifferences plot throws an error
+# test_that("createCritDifferences for mlr benchmark", {
+#   p = createCritDifferencesPlot(microbenchmark.example, "measure.min")
+#   expect_true(is.list(p))
+# })
